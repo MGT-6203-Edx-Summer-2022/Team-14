3d87f246e8ac5ec8549dd04c2882ce59ba313f77
Bao Vo <baovo0293@gmail.com>
2022-07-11 00:14:23 -0700

    adding historical SP500 institutions for 12 period

--- Recession-Proof_Portfolio/Scripts/get_SP500_historical_data.R
+++ Recession-Proof_Portfolio/Scripts/get_SP500_historical_data.R
@@ -1,6 +1,8 @@
 # Load dependencies
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(tidyverse, rvest)
+library(dplyr)
+library(readr)
 
 wikispx <- read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
 currentconstituents <- wikispx %>%
@@ -24,7 +26,7 @@ spxchanges
 currentmonth <- as.Date(format(Sys.Date(), '%Y-%m-01'))
 monthseq <- seq.Date(as.Date('1969-04-01'), currentmonth, by = 'month') %>% rev()
 
-spxstocks <- currentconstituents %>% mutate(Date = currentmonth) %>% select(Date, Ticker = Symbol, Name = Security)
+spxstocks <- currentconstituents %>% mutate(Date = currentmonth) %>% select(Date, Ticker = Symbol, Name = Security, Sector = `GICS Sector`)
 lastrunstocks <- spxstocks
 
 # Iterate through months, working backwards
@@ -52,5 +54,33 @@ for (i in 2:length(monthseq)) {
   
   lastrunstocks <- thismonth
 }
-current_m <- filter(spxstocks, Date >= '1969-04-01' & Date <= '1971-01-01')
-current_m
+
+# use for period 1969-04-01 to 1971-01-01
+sp500_1971 <- filter(spxstocks, Date == '1971-01-01')
+
+# use for period 1973-10-01 to 1975-04-01
+sp500_1975 <- filter(spxstocks, Date == '1975-04-01')
+
+# use for period 1979-04-01 to 1980-07-01
+sp500_1980 <- filter(spxstocks, Date == '1980-07-01')
+
+# use for period 1981-04-01 to 1982-07-01
+sp500_1982 <- filter(spxstocks, Date == '1982-07-01')
+
+# use for period 1989-10-01 to 1991-04-01
+sp500_1991 <- filter(spxstocks, Date == '1991-04-01')
+
+# use for period 2001-01-01 to 2001-10-01
+sp500_2001 <- filter(spxstocks, Date == '2001-10-01')
+
+# use for period 2007-10-01 to 2009-07-01
+sp500_2007 <- filter(spxstocks, Date == '2009-07-01')
+
+# use for period 2020-01-01 to 2020-07-01
+sp500_2020 <- filter(spxstocks, Date == '2020-07-01')
+
+combined <- do.call("rbind", list(sp500_1971, sp500_1975, sp500_1980, sp500_1982, sp500_1991, sp500_2001, sp500_2007, sp500_2020))
+
+# write.csv(as.data.frame(combined), "/Users/baovo/Documents/GitHub/Team-14/Recession-Proof_Portfolio/Data/sp500_bear_periods.csv")
+
+
